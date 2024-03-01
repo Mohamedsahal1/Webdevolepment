bc38150329e4eacca867bd305ae5be3d9dfea3d2
Mohamedsahal1 <mohamed.sahalmohamed@student.vives.be>
2024-03-01 22:44:40 +0100

    l

--- labo_15/Producten/scripts/code.js
+++ labo_15/Producten/scripts/code.js
@@ -9,15 +9,15 @@ let pri =document.getElementsByClassName("prijs");
 let btw = document.getElementsByClassName("btw");
 let subtot =document.getElementsByClassName("subtotaal");
 
-subtot[0].innerHTML = parseInt(pri[0].innerHTML, 10) * parseInt(aant[0].value, 10) +
-    (parseInt(pri[0].innerHTML, 10) * parseInt(aant[0].value, 10) * parseInt(btw[0].innerHTML, 10)/100)
-               +" " +"Eur";
-subtot[1].innerHTML = parseInt(pri[1].innerHTML, 10) * parseInt(aant[1].value, 10) +
-    (parseInt(pri[1].innerHTML, 10) * parseInt(aant[1].value, 10) * parseInt(btw[1].innerHTML, 10)/100)
-    +" " +"Eur";
-subtot[2].innerHTML = parseInt(pri[2].innerHTML, 10) * parseInt(aant[2].value, 10) +
-    (parseInt(pri[2].innerHTML, 10) * parseInt(aant[2].value, 10) * parseInt(btw[2].innerHTML, 10)/100)
-    +" " +"Eur";
+subtot[0].innerHTML = (parseInt(pri[0].innerHTML, 10) * parseInt(aant[0].value, 10) +
+    (parseInt(pri[0].innerHTML, 10) * parseInt(aant[0].value, 10) * parseInt(btw[0].innerHTML, 10)/100)).toFixed(2)
+               +" " +"Eur ";
+subtot[1].innerHTML = (parseInt(pri[1].innerHTML, 10) * parseInt(aant[1].value, 10) +
+    (parseInt(pri[1].innerHTML, 10) * parseInt(aant[1].value, 10) * parseInt(btw[1].innerHTML, 10)/100)).toFixed(2)
+    +" " +"Eur ";
+subtot[2].innerHTML = (parseInt(pri[2].innerHTML, 10) * parseInt(aant[2].value, 10) +
+    (parseInt(pri[2].innerHTML, 10) * parseInt(aant[2].value, 10) * parseInt(btw[2].innerHTML, 10)/100)).toFixed(2)
+    +" " +"Eur ";
 
 
 let eind = (parseInt(pri[0].innerHTML, 10) * parseInt(aant[0].value, 10) +
@@ -28,7 +28,7 @@ let eind = (parseInt(pri[0].innerHTML, 10) * parseInt(aant[0].value, 10) +
     (parseInt(pri[2].innerHTML, 10) * parseInt(aant[2].value, 10) * parseInt(btw[2].innerHTML, 10)/100));
 
 
-    document.getElementById("subtotaalLaatste").innerHTML = eind + " " + "Eur";
+    document.getElementById("subtotaalLaatste").innerHTML = eind.toFixed(2) + " " + "Eur ";
 
 }
 window.addEventListener("load", setup);
