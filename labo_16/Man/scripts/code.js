8f603c0af60c776658c7779a706db2c1a22975d9
Mohamedsahal1 <mohamed.sahalmohamed@student.vives.be>
2024-03-09 21:18:21 +0100

    dfqsfqd

--- labo_16/Man/scripts/code.js
+++ labo_16/Man/scripts/code.js
@@ -1,6 +1,10 @@
 const setup = () => {
     let b = document.getElementById("button");
     b.addEventListener("click", breken);
+    let p = document.getElementById("button1");
+    p.addEventListener("click", breken1);
+    let t = document.getElementById("buttonTest");
+    t.addEventListener("click", test);
 
 }
 
@@ -13,7 +17,29 @@ const breken=()=>{
         aantal++;
         index = statement.indexOf(gezochtstatement, index+1);
     }
-    console.log(aantal);
+    document.getElementById("result1").textContent += aantal;
+}
+const breken1=()=>{
+    let statement = "De man van An geeft geen hand aan ambetante verwanten";
+    let gezochtstatement = "an";
+    let aantal = 0;
+    let index = statement.lastIndexOf(gezochtstatement);
+    while(index != -1){
+        aantal++;
+        index = statement.lastIndexOf(gezochtstatement, index-1);
+    }
+    document.getElementById("result2").textContent += aantal;
+}
+const test=()=>{
+    let statement = document.getElementById("prob1").value;
+    let gezochtstatement = document.getElementById("prob2").value;
+    let aantal = 0;
+    let index = statement.indexOf(gezochtstatement);
+    while(index != -1){
+        aantal++;
+        index = statement.indexOf(gezochtstatement, index+1);
+    }
+    document.getElementById("try").textContent += aantal;
 }
 
 window.addEventListener("load", setup);
