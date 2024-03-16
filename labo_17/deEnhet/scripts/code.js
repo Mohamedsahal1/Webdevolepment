dd8d77b59d5807c45c70179ed0316465a6fa461c
Mohamedsahal1 <mohamed.sahalmohamed@student.vives.be>
2024-03-16 21:12:02 +0100

    g

--- labo_17/deEnhet/scripts/code.js
+++ labo_17/deEnhet/scripts/code.js
@@ -4,15 +4,38 @@ document.getElementById("output").addEventListener("click",methode)
 
 const methode=()=>{
     let input = document.getElementById("textinput").value.trim();
-
-    for(let i = 0; i < 1; i++){
-        let resultaat= input
+    let indhet;
+    let indHet;
+    let resultaat;
+    for(let i = 0; i < 1; i++) {
+        resultaat = input
             .replace("de", "het")
             .replace("De", "Het")
-            .replace("het", "de")
-            .replace("Het", "De");
-        console.log(resultaat);
+        indhet = resultaat.indexOf("het");
+        indHet = resultaat.indexOf("Het");
     }
+
+    for(let m = 0; m < 1; m++){
+        for(let a =0; a < 1; a++){
+            if(m !== indhet[a]) {
+                input.replace("het", "de")
+            }
+        }
+    }
+    for(let s = 0; s < 1; s++){
+        for(let b =0; b < 1; b++){
+            if(s !== indhet[b]) {
+                input.replace("Het", "de");
+            }
+        }
+    }
+
+    console.log(resultaat);
+    console.log(indhet);
+    console.log(indHet);
+
+
+
 
 }
 
